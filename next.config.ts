19e11446e20f87185a3186bb13d6c280280a93d6
Djatiaja <djamgt23@gmail.com>
2026-08-05 12:56:33 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,7 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
+    unoptimized: true,
   },
   // Increase body size limit for file uploads
   experimental: {
